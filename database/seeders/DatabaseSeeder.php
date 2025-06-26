569ce638b4b2cb61d90e7ba5b4be71b1a895fc9d
goto-masa <68987213+goto-masa@users.noreply.github.com>
2025-06-26 12:06:04 +0900

    Add prices table and related model, factory, seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use App\Models\Hotel;
+use App\Models\Price;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -17,6 +18,7 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         Hotel::factory(5)->create();
+        Price::factory(5)->create();
 
         User::factory()->create([
             'name' => 'Test User',
