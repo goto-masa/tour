817783aa7a7f76ab1e2539c242aa1c6b9fb234f1
goto-masa <68987213+goto-masa@users.noreply.github.com>
2025-06-26 11:41:19 +0900

    Add Hotel model and factory

---
+++ app/Models/Hotel.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+/**
+ * @use HasFactory<\Database\Factories\HotelFactory>
+ */
+class Hotel extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'name',
+        'address',
+        'tel',
+        'contact',
+        'lang',
+        'note',
+    ];
+}
