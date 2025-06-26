1d530ad714653295c671eccfb2a4967d45a1d29f
goto-masa <68987213+goto-masa@users.noreply.github.com>
2025-06-26 12:30:22 +0900

    Add admins table, model, factory and seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use App\Models\Hotel;
+use App\Models\Admin;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -21,6 +22,10 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+        ]);
+
+        Admin::factory()->create([
+            'name' => 'Admin User',
         ]);
     }
 }
